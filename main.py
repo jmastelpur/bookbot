165dc3f25687aa15092a8dae6cf73b3ad500ed69
IgneelX777 <jmastelpur@gmail.com>
2025-08-14 13:07:43 -0400

    Main.py blueprint

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("greetings boots")
+def get_book_text(file_path):
 
+    with open(file_path) as f:
+        text = f.read()
+
+    return text
+
+
+def main():
+    book_text = get_book_text("/home/jmast/workspace/github.com/Jmastelpur/bookbot/frankenstein.txt")
+    print(book_text)
