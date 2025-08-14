5cebaf4e47f6022d20f9db866d4d0dc9b9f18ac0
IgneelX777 <jmastelpur@gmail.com>
2025-08-14 15:33:39 -0400

    works on inputed path/finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count, number_of_characters, sorted_dictionaries
+import sys
 
 def get_book_text(file_path):
 
@@ -8,12 +9,18 @@ def get_book_text(file_path):
     return text
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
     number = word_count(text)
     characters = number_of_characters(text)
     characters_sorted = sorted_dictionaries(characters)
     print("============ BOOKBOT ============\n")
-    print(f"Analyzing book found at books/frankenstein.txt\n")
+    print(f"Analyzing book found at {book_path}\n")
     print("----------- Word Count ----------\n")
     print(f"Found {number} total words\n")
     print("----------- Character Count -----------\n")
